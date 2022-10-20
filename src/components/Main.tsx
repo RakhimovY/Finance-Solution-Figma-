ebe0b474f5ebcf6a8131de0f90bc406689a13076
RakhimovY <rakhimov.y.hh@gmail.com>
2022-10-20 12:05:17 +0600

    Start

---
+++ src/components/Main.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+
+type Props = {};
+
+const Main = (props: Props) => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default Main;
